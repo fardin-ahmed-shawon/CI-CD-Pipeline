11aaea0e9819cabd74e071ed898dbc7665a6f48c
Fardin Ahmed <fardinahmed7443@gmail.com>
2026-02-14 11:27:29 +0600

    push from test branch

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CI-CD-TEST</title>
-</head>
-<body>
-    <h1>CI-CD-TEST</h1>
-</body>
-</html>
+<?php
+ echo "Hello, World! This is a CI/CD test.";
+?>
